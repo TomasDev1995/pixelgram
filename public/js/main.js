d4cf557a54ce4e37578da27193950a386114d70b
Tomas <tomas.e.aguilera95@gmail.com>
2021-09-11 05:24:42 -0300

    Like's system

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+var url = 'http://pixelgram.com';
+window.addEventListener("load", function(){
+
+   $('.btn-like').css('cursor', 'pointer');
+   $('.btn-dislike').css('cursor', 'pointer');
+
+   //Botón de like
+    function like(){
+        $('.btn-like').unbind('click').click(function(){
+            console.log('like');
+            $(this).addClass('btn-dislike').removeClass('btn-like');
+            $(this).attr('src', url+'/img/corazon-rojo.png');
+            $.ajax({
+                url: url+'/like/'+$(this).data('id'),
+                type:'GET',
+                success: function(response){
+                    if (response){
+                        console.log('Has dado like a la publicacion');
+                    }else{
+                        console.log('Error al dar like');
+                    }
+                }
+            });
+            dislike();
+        });
+    }
+    like();
+
+    function dislike(){
+        $('.btn-dislike').unbind('click').click(function(){
+            console.log('dislike');
+            $(this).addClass('btn-like').removeClass('btn-dislike');
+            $(this).attr('src', url+'/img/corazon-negro.png');
+            $.ajax({
+                url: url+'/dislike/'+$(this).data('id'),
+                type:'GET',
+                success: function(response){
+                    if (response){
+                        console.log('Has dado dislike a la publicacion');
+                    }else{
+                        console.log('Error al dar dislike');
+                    }
+                }
+            });
+            like();
+        });
+    }
+    dislike();
+
+
+});
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         echo $image->imagen_path. "<br/>";
         echo $image->description. "<br/>";
         echo $image->user->name." ".$image->user->surname."<br/>";
-        
+
         if(count($image->comments) >= 1){
             echo "<h4>Comentarios</h4>";
             foreach($image->comments as $comment){
@@ -55,5 +55,8 @@ Route::get('/image/file/{filename}', [App\Http\Controllers\ImageController::clas
 Route::get('/image/{id}', [App\Http\Controllers\ImageController::class, 'detail'])->name('image.detail');
 Route::post('/comment/save', [App\Http\Controllers\CommentController::class, 'save'])->name('comment.save');
 Route::get('/comment/delete/{id}', [App\Http\Controllers\CommentController::class, 'delete'])->name('comment.delete');
+Route::get('/like/{image_id}', [App\Http\Controllers\LikeController::class, 'like'])->name('like.save');
+Route::get('/dislike/{image_id}', [App\Http\Controllers\LikeController::class, 'dislike'])->name('like.delete');
+Route::get('/likes', [App\Http\Controllers\LikeController::class, 'index'])->name('likes.index');
 
 
